d2252bd76d057edaae3759735b381cf6fec2607c
Catalin Ramascanu <catalin.rmc@gmail.com>
2018-07-07 11:43:02 -0500

    129. Added AI Tank Controller

--- Source/BattleTanks/TankPlayerController.cpp
+++ Source/BattleTanks/TankPlayerController.cpp
@@ -3,5 +3,24 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto controlledTank = getControlledTank();
+	if (!controlledTank)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Failed to get controlled Tank."));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Managed to get the controlled tank: %s"), *(controlledTank->GetName()));
+	}
+}
+
+ATank* ATankPlayerController::getControlledTank() {
+	return Cast<ATank>(GetPawn());
+}
+
+
 
 
--- Source/BattleTanks/TankPlayerController.h
+++ Source/BattleTanks/TankPlayerController.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "Public/Tank.h"
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "TankPlayerController.generated.h"
@@ -13,8 +14,11 @@ UCLASS()
 class BATTLETANKS_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
-	
+
+public:
+	ATank* getControlledTank();
+
+	void BeginPlay() override;
 	
 	
 };
